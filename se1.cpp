d0d7e2bfb76e69a2022bb65d486866f543cf01ea
hwkl <hanwook0324@gmail.com>
2023-04-18 11:28:09 +0900

    fix

--- se1.cpp
+++ se1.cpp
@@ -2,6 +2,16 @@
 #include <vector>
 #include <set>
 
+class Driver {
+private:
+	int salary;
+	int workhour;
+	int wage;
+public:
+	Driver(int workhour, int wage) {this->workhour = workhour, this->wage = wage};
+	int getSalary() {};
+};
+
 int main() {
   int N;
   std::cin >> N;
